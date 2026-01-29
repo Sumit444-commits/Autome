5c6c2d5f60b776ced219e3d6d33a97014f8de4c0
sumit444-commits <sumit8444061@gmail.com>
2026-01-29 22:56:12 +0500

    cors updated

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5000", "https://autome-3aq5.onrender.com"],
+    origin: ["http://localhost:5173", "http://localhost:5000","https://autome.vercel.app", "https://autome-3aq5.onrender.com"],
     credentials: true,
   }),
 );
